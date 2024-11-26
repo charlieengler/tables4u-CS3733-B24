688cc4800f12ce16b23cfffeff6e77659b01d95e
CharlieEngler3 <charlieengler3@gmail.com>
2024-11-25 19:53:59 -0500

    Added basic working state of restaurant manager page

--- src/app/routes/restaurants.ts
+++ src/app/routes/restaurants.ts
@@ -4,7 +4,7 @@ import createInstance from "./instance";
 
 export function createRestaurant(email: string, username: string, password: string, restaurantName: string, address: string, cityState: string, zipcode: string) {
     return new Promise((resolve, reject) => {
-        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Update-1');
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
 
         const city = cityState.split(',')[0];
         const state = cityState.split(', ')[1];
@@ -42,15 +42,201 @@ export function createRestaurant(email: string, username: string, password: stri
     });
 }
 
+export function deleteRestaurant(uid: number, eid: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        // TODO: Should probably check to make sure the person deleting it has the proper credentials
+        instance.post('/delete-restaurant', {restaurant: uid}).then(res => {
+            if(res.data.statusCode == 200)
+                resolve('Successfully deleted restaurant.');
+            else
+                reject(res.data.body.error);
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+export function generateReport(uid: number, date: string) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/gen-report', {uid: uid, date: date}).then(res => {
+            if(res.data.statusCode == 200) {
+                resolve(res.data.body);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
 export function getRestaurantById(uid: number) {
     return new Promise((resolve, reject) => {
-        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Update-1');
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
 
         instance.post('/get-restaurant/id', {uid: uid}).then(res => {
             if(res.data.statusCode == 200) {
                 const restaurant = res.data.body;
 
                 resolve(restaurant);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+export function getRestaurantByManager(eid: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/get-restaurant/manager', {eid: eid}).then(res => {
+            if(res.data.statusCode == 200) {
+                const restaurant = res.data.body;
+
+                resolve(restaurant);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+export function getSchedule(sid: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/get-restaurant/schedule', {sid: sid}).then(res => {
+            if(res.data.statusCode == 200) {
+                const schedule = res.data.body.schedule;
+
+                resolve(schedule);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    })
+}
+
+export function toggleActivation(uid: number, eid: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/set-restaurant/toggle-activation', {uid: uid, eid: eid}).then(res => {
+            if(res.data.statusCode == 200) {
+                const status = res.data.body.status;
+
+                resolve(status);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    })
+}
+
+export function updateClosingDates(uid: number, closings: string[]) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/set-restaurant/closings', {uid: uid, dates: closings}).then(res => {
+            if(res.data.statusCode == 200) {
+                const dates = res.data.body.dates;
+
+                resolve(dates);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+export function updateRestaurantDetails(uid: number, eid: number, email: string, username: string, password: string, name: string, address: string, cityState: string, zipcode: string) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/set-restaurant/details', {
+            uid: uid,
+            eid: eid,
+            email: email,
+            username: username,
+            password: password,
+            name: name,
+            address: address,
+            city: cityState.split(',')[0],
+            state: cityState.split(', ')[1],
+            zipcode: zipcode,
+        }).then(res => {
+            if(res.data.statusCode == 200) {
+                const details = res.data.body;
+
+                resolve(details);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+export function updateScheduleTimes(sid: number, opening: number, closing: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/set-restaurant/schedule', {sid: sid, opening: opening, closing: closing}).then(res => {
+            if(res.data.statusCode == 200) {
+                const status = res.data.body.message;
+
+                resolve(status);
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+export function updateTableCount(seatCount: number, numTables: number, uid: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://h3q7tcd7ji.execute-api.us-east-2.amazonaws.com/Development');
+
+        instance.post('/set-restaurant/tables', {seatCount: seatCount, numTables: numTables, uid: uid}).then(res => {
+            console.log(res)
+            if(res.data.statusCode == 200) {
+                const tables = res.data.body.tables;
+
+                resolve(tables);
             } else {
                 const error = res.data.body.error;
 
--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -2,6 +2,30 @@ import secureLocalStorage from 'react-secure-storage';
 
 import createInstance from './instance';
 
+export function getUserById(eid: number) {
+    return new Promise((resolve, reject) => {
+        const instance = createInstance('https://on7l6qgobb.execute-api.us-east-2.amazonaws.com/Initial')
+
+        instance.post('/get-user-by-id', {eid: eid}).then(res => {
+            if(res.data.statusCode == 200) {
+                const email = res.data.body.email;
+                const username = res.data.body.username;
+
+                resolve({
+                    email: email,
+                    username: username,
+                });
+            } else {
+                const error = res.data.body.error;
+
+                reject(error);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    })
+}
+
 export function login(username: string, password: string) {
     return new Promise((resolve, reject) => {
         const instance = createInstance('https://61f3l9b77g.execute-api.us-east-2.amazonaws.com/Initial');
@@ -9,12 +33,19 @@ export function login(username: string, password: string) {
         instance.post('/login', {'username': username, 'password': password}).then(res => {
             if(res.data.statusCode == 200) {
                 const token = res.data.body.token;
+                const eid = res.data.body.eid;
+                const username = res.data.body.username;
                 const redirect = res.data.body.redirect;
 
                 // TODO: If the user is a restaurant manager, also fetch the restaurant they manage
                 secureLocalStorage.setItem('token', token);
+                secureLocalStorage.setItem('username', username);
+                secureLocalStorage.setItem('eid', eid);
 
-                resolve(redirect);
+                resolve({
+                    eid: eid,
+                    redirect: redirect,
+                });
             } else {
                 const error = res.data.body.error;
 
@@ -30,8 +61,8 @@ export function logout() {
     return new Promise((resolve, reject) => {
         try {
             secureLocalStorage.removeItem('token');
-            if(secureLocalStorage.getItem('uid'))
-                secureLocalStorage.removeItem('uid');
+            secureLocalStorage.removeItem('uid');
+            secureLocalStorage.removeItem('username');
 
             resolve('Successfully logged out.');
         } catch (err) {
