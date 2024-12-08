c19b77dbbd4c854b7839775f437cb24dd8b3df84
AndrewR15 <49137401+AndrewR15@users.noreply.github.com>
2024-12-08 12:06:18 -0500

    Update page.tsx
    Removed extra /

--- src/app/(pages)/CreateRestaurant/page.tsx
+++ src/app/(pages)/CreateRestaurant/page.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 import secureLocalStorage from 'react-secure-storage';
 
-import Alert from '../..//components/Alert';
+import Alert from '../../components/Alert';
 import Banner from '../../components/Banner';
 import RestaurantDetails from '../../components/RestaurantDetails';
 
