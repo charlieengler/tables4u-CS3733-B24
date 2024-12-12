54b8e8db9d55f63a3dd9f75b650060b3e9af1780
CharlieEngler3 <charlieengler3@gmail.com>
2024-12-12 10:30:08 -0500

    Updated consumer page styling to be inline with restaurant manager

--- src/app/(pages)/Consumer/page.tsx
+++ src/app/(pages)/Consumer/page.tsx
@@ -87,8 +87,7 @@ export default function Consumer() {
                         ...prevTimes,
                         [restaurant]: times,
                     }));
-                }
-                catch (err) {
+                } catch (err) {
                     showNotification("No Available Tables", 5000)
                     console.log(err)
                 }
@@ -97,8 +96,6 @@ export default function Consumer() {
     }
 
     const findRes = async () => {
-        
-        
         try {
             const resData = await findReservation(email, confCode) as Reservation
             
@@ -159,15 +156,13 @@ export default function Consumer() {
     const handleTimeSelect = async (restaurant: string, time: number) => {
         setTime(String(time))
         setRestaurant(restaurant)
-        setSelectedTime((prev) => ({
-            ...prev,
+        setSelectedTime(() => ({
             [restaurant]: time, // Store the selected time for the specific restaurant
         }));
     }
 
     const isSelected = (restaurant: string, time: number) => {
         return selectedTime[restaurant] === time;
-        
     };
 
     const createResClick = async () => {
@@ -189,59 +184,64 @@ export default function Consumer() {
     return (
         <>
             <Banner/>
-            <div className={styles.searchContainer}>
-                <input className={styles.searchInput} onChange={e => setDate(e.target.value)} placeholder="Date: YYYY-MM-DD" type="text" value={date}></input>
-                <input className={styles.searchInput} onChange={e => setTime(e.target.value)} placeholder="Time: 24HR" type="text" value={time}></input>
-                <input className={styles.searchInput} onChange={e => setGuests(e.target.value)} placeholder="Number of Guests" type="text" value={guests}></input>
-                <input className={styles.searchInput} onChange={e => setEmail(e.target.value)} placeholder="Email" type="text" value={email}></input>
-                <select className={styles.dropdown} onChange={e => setRestaurant(e.target.value)} value={restaurant}>
-                    <option value="" disabled>Select a Restaurant</option>
-                    {restaurantList?.map((option, index) => (
-                        <option key={index} value={option}>
-                            {option}
-                        </option>
-                    ))}</select>
-                <button className={styles.searchButton} onClick={findTables}>Search</button>
-            </div>
-
-            
-
-            <div className={styles.restaurantContainer}>
-            <button className={styles.createResButton} onClick={createResClick}>Create Reservation</button>
-            {restaurantList.length > 0 ? restaurantList.map((restaurant, index) => {
-                return (
-                    <div key={index} className={styles.restaurantItem}>
-                        <div>{restaurant}</div>
-                        <div>
-                            {getRestaurantTime(restaurant).map((time, timeIndex) => (
-                                <button
-                                    key={`${restaurant}-${time}`}
-                                    onClick={() => handleTimeSelect(restaurant, time)}
-                                    className={`${styles.timeButton} ${isSelected(restaurant, time) ? styles.selected : ''}`}
-                                >
-                                    {time}
-                                </button>
+            <div className={styles.consumerContainer}>
+                <div className={styles.searchFindContainer}>
+                    <div className={styles.searchContainer}>
+                        <div className={styles.containerTitle}>Restaurant Search</div>
+                        <input className={styles.searchInput} onChange={e => setDate(e.target.value)} placeholder="Date: YYYY-MM-DD" type="text" value={date}></input>
+                        <input className={styles.searchInput} onChange={e => setTime(e.target.value)} placeholder="Time: 24HR" type="text" value={time}></input>
+                        <input className={styles.searchInput} onChange={e => setGuests(e.target.value)} placeholder="Number of Guests" type="text" value={guests}></input>
+                        <input className={styles.searchInput} onChange={e => setEmail(e.target.value)} placeholder="Email" type="text" value={email}></input>
+                        <select className={styles.dropdown} onChange={e => setRestaurant(e.target.value)} value={restaurant}>
+                            <option value="" disabled>Select a Restaurant</option>
+                            {restaurantList?.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
                             ))}
-                        </div>
+                        </select>
+                        <button className={styles.searchButton} onClick={findTables}>Search</button>
                     </div>
-                )}) : (restaurantList.length == 0 ? 
-                <div>No Active Restaurants</div> 
-                : <div>Loading Restaurants...</div>)}
-            </div>
 
-            <div className={styles.findReservationContainer}>
-                <input className={styles.searchInput} onChange={e => setEmail(e.target.value)} placeholder="Email" type="text" value={email}></input>
-                <input className={styles.searchInput} onChange={e => setConfCode(e.target.value)} placeholder="Confirmation Code" type="text" value={confCode}></input>
-                <div className={styles.reservationSubcontainer}>
-                    <div className={styles.reservationInfo}>{reservation ? reservation.restaurant + " on " + reservation.date + " at " + reservation.time + " for " + reservation.guests + " guests" : ''}</div>
+                    <div className={styles.findReservationContainer}>
+                        <div className={styles.containerTitle}>Reservation Search</div>
+                        <input className={styles.searchInput} onChange={e => setEmail(e.target.value)} placeholder="Email" type="text" value={email}></input>
+                        <input className={styles.searchInput} onChange={e => setConfCode(e.target.value)} placeholder="Confirmation Code" type="text" value={confCode}></input>
+                        <div className={styles.reservationSubcontainer}>
+                            <div className={styles.reservationInfo}>{reservation ? reservation.restaurant + " on " + reservation.date + " at " + reservation.time + " for " + reservation.guests + " guests" : ''}</div>
+                        </div>
+                        <button className={styles.findReservationButton} onClick={findRes}>Find Reservation</button>
+                        <button className={styles.cancelReservationButton} onClick={cancelRes}>Cancel Reservation</button>
+                    </div>
                 </div>
-                <button className={styles.findReservationButton} onClick={findRes}>Find Reservation</button>
-                <button className={styles.cancelReservationButton} onClick={cancelRes}>Cancel Reservation</button>
-                
+
+                <div className={styles.restaurantContainer}>
+                    <button className={styles.createResButton} onClick={createResClick}>Create Reservation</button>
+                    {restaurantList.length > 0 ? restaurantList.map((restaurant, index) => {
+                        return (
+                            <div key={index} className={styles.restaurantItem}>
+                                <div className={styles.restaurantTitle}>{restaurant}</div>
+                                <div className={styles.restaurantTimesContainer}>
+                                    {getRestaurantTime(restaurant).length > 0 ? getRestaurantTime(restaurant).map((time, timeIndex) => (
+                                        <button
+                                            key={`${restaurant}-${time}`}
+                                            onClick={() => handleTimeSelect(restaurant, time)}
+                                            className={`${styles.timeButton} ${isSelected(restaurant, time) ? styles.selected : ''}`}
+                                        >
+                                            {time}
+                                        </button>
+                                    )) : <div>Please search for a reservation time, date, and guest count.</div>}
+                                </div>
+                            </div>
+                        )}) : (restaurantList.length == 0 ? 
+                            <div>No Active Restaurants</div> 
+                            :
+                            <div>Loading Restaurants...</div>
+                        )}
+                </div>
             </div>
+            
             <NotificationBox visible={visible} text={text} />
-
-
         </>
     );
 }
